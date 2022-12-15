f2fba444582341a0f3374075ab044b1be7211a49
Firas Jerbi <34075959+fjerbi@users.noreply.github.com>
2022-12-15 03:10:32 -0800

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 
 
 const config={
-    apiKey: "AIzaSyAwS-fS-vgDNIMWKYqNIigwEwtXbSN7dAU",
-    authDomain: "andromeda-store.firebaseapp.com",
-    databaseURL: "https://andromeda-store.firebaseio.com",
-    projectId: "andromeda-store",
-    storageBucket: "andromeda-store.appspot.com",
-    messagingSenderId: "19535294008",
-    appId: "1:19535294008:web:38db92e65da99cf6cf00cd",
-    measurementId: "G-1PQLPYLB86"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 
